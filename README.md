df261dceba1e63b01303153571a2173fe06f4bf1
crypt <293264906+thecryptidcat@users.noreply.github.com>
2026-09-09 04:31:36 +0100

    new stuff
    added more to code and filled out readme

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-# laptop-spec-map
-a program to show a customisable map of laptops displaying their core specs
+# Laptop specification map
+A program to show a customisable map of laptops displaying their core specs
 
-in progress so code will likely not run as is
+This is more of a tool I am working on for personal use than a project to showcase, but I'm uploading it here for ease of access and editing so feel free to follow along in its progress. Do
+
+note: each chunk of edits may make the code nonfunctional as I save from each session whether I have finished the part I was working on or not
+
+---
+
+### About
+
+I'm creating this tool in order to hold and save locations of laptops in a display, showing their rough specs and code to look them up further. Eventually I will add the ability to save configurations, but that's going to be a whole ordeal to learn in itself, so for now it's just the foundation of the GUI.
+
+I know I could probably look up online for a simple enough graphic design program to do this, but I thought I'd try take it on as a challenge to test my skill and learn more. By the time this is done, the purpose for which I'm making it may be obsolete, but I'd have learned a lot so it's worth it anyway.
--- laptop-spec-map.py
+++ laptop-spec-map.py
@@ -2,109 +2,152 @@ import customtkinter as ctk
 import json
 import os
 
+testlist = ["asus zenbook 14", "lenovo yoga", "hp omnibook"]
 
 laptops = []
+laptop_names = []
 good_laptops = []
+
+dropdownlist = []
+
 
 laptops = [
     {"name": "asus zenbook 14",
+    "sku": "n/a",    
     "processor": "int ult 5",
     "ram": "16gb ddr5",
     "storage": "512gb",
     "rating": 7.5
     }
-    ]
+]
 
+reference_dict = laptops[0]
+
+
+laptop_names = [laptop["name"] for laptop in laptops]
+
+root = ctk.CTk()
+root.title("laptop layout")
+root.geometry("1600x950")
+
+title = ctk.CTkLabel(root, text="laptop layout map")
+title.pack(side=ctk.TOP)
+
+centre_frame = ctk.CTkFrame(root)
+centre_frame.pack(side=ctk.TOP)
+
+output_label = ctk.CTkLabel(root, text="")
+output_label.pack(side=ctk.TOP)
+
+def create_laptop_frame(masterroot):
+    frame = ctk.CTkFrame(masterroot)
+
+    frame.grid_rowconfigure(0, weight=0)
+
+    labels = []
+
+    def set_laptop(choice):
+                laptopchoice = next((laptop for laptop in laptops if laptop.get("name") == choice), None)
+                output_text = ""
+                for key, value in laptopchoice.items():
+                     output_text+=(f"{key}: {value} \n")
+                     output_label.configure(text=(output_text))
+                for i, (key, value) in enumerate(laptopchoice.items()):
+                     value_text = (f"{key}: {value}")
+                     labels[i].configure(text=value_text)
+
+    dropdownbox = ctk.CTkOptionMenu(master = frame, values=laptop_names, font=("Helvetica", 10), width=30, height=15, command = set_laptop)
+    dropdownbox.pack(side=ctk.TOP)
+    dropdownlist.append(dropdownbox)
+
+    innerframe = ctk.CTkFrame(frame)
+    innerframe.pack(side=ctk.TOP)
+
+    for i, key,in enumerate(reference_dict):
+         label = ctk.CTkLabel(innerframe, text=f"{key}:", font=("Helvetica", 10), height=13)
+         label.grid(row=i+1, column=0)
+         labels.append(label)
+
+    
+
+    return frame
+
+def update_dropdowns():
+     for dropdown in dropdownlist:
+          dropdown.configure(values=laptop_names)
 
 def create_new_laptop():
     new_laptop = {"name": "",
+                  "sku": "",
                   "processor": "",
                   "ram": "",
                   "storage": "",
                   "rating": 0
                   }
     for key in new_laptop:
-        new_laptop[key] = input(f"What is the {key} of the laptop? /")
+        new_laptop[key] = input(f"What is the {key} of the laptop? \n")
     laptops.append(new_laptop)
+    laptop_names.append(new_laptop["name"])
+    update_dropdowns()
+
     
 def test_display_laptop(laptop):
     for item in laptop:
         print(laptop[item])
 
 
+def create_table_frame(masterroot, columns, rows):
+    frame = ctk.CTkFrame(masterroot)
 
-def create_frame(master):
-    frame = ctk.CTkFrame(master)
+    frame.grid_columnconfigure(0, weight=1)
 
-    def set_laptop(choice):
-                laptopchoice = next((laptop for laptop in laptops if laptop.get("name") == choice), None)
-                print(laptopchoice)
-
-    dropdownbox = ctk.CTkOptionMenu(frame, values=[laptops], command=set_laptop)
-    dropdownbox.pack(side=ctk.TOP)
-
-    innerframe = ctk.CTkFrame(frame)
-    innerframe.pack(side=ctk.TOP)
-
-    
-
+    for row in range(rows):
+         for column in range(columns):
+              laptopslot = create_laptop_frame(frame)
+              laptopslot.grid(row=row, column=column)
 
     return frame
+    
 
-
-
-
-root = ctk.CTk()
-root.title("laptop layout")
-root.geometry("1400x800")
-
-title = ctk.CTkLabel(root, text="laptop layout map")
-title.pack(side=ctk.TOP)
-
-# -------------------------------------------------------------
-
-centre_frame = ctk.CTkFrame(root)
-centre_frame.pack(side=ctk.TOP)
-
-# -----------------------------------------
-
-# top leftmost frame
-
-topll_frame = ctk.CTkFrame(centre_frame)
-topll_frame.grid(row=0, column=0)
-
-
-
-# -----------------------------------
-
-# top centreleft frame
-
-topl_frame = ctk.CTkFrame(centre_frame)
-topl_frame.grid(row=0, column=1)
-
-# -----------------------------------
-
-# top centreright frame
-
-create_frame(centre_frame)
+# ----------------
 
 
 # -----------------------------------
 
 # top rightmost frame
 
+toprr_frame = create_table_frame(centre_frame, 3, 2)
+toprr_frame.grid(row=0, column=3, padx=5, pady=5)
 
 
-# ---------------------------------------
 
-# intel table frame
 
 
 
 # -----------------------------------
 
-# microsoft table frame
+# top centreright frame
 
+topr_frame = create_table_frame(centre_frame, 3, 2)
+topr_frame.grid(row=0, column=2, padx=5, pady=5)
+
+# -----------------------------------------
+
+# top centreleft frame
+
+topl_frame = create_table_frame(centre_frame, 3, 2)
+topl_frame.grid(row=0, column=1, padx=5, pady=5)
+
+# ---------------------------------------
+
+# top leftmost frame
+
+topll_frame = create_table_frame(centre_frame, 3, 2)
+topll_frame.grid(row=0, column=0, padx=5, pady=5)
+
+# -----------------------------------
+
+# middle rightmost frame
 
 
 # -----------------------------------
@@ -112,17 +155,19 @@ create_frame(centre_frame)
 # middle centre frame
 
 
+# -----------------------------------
+
+# microsoft table frame
+
 
 # -----------------------------------
 
-# middle rightmost frame
-
+# intel table frame
 
 
 # ---------------------------------------
 
-# bottom centreleft frame
-
+# bottom rightmost frame
 
 
 # -----------------------------------
@@ -130,21 +175,18 @@ create_frame(centre_frame)
 # bottom centreright frame 
 
 
-
 # -----------------------------------
 
-# bottom rightmost frame
-
+# bottom centreleft frame
 
 
 # --------------------------------------------------------
 
+#create_new_laptop()
+#print(laptop_names)
 
+print(dropdownlist)
 
-
-
-good_laptops = [laptop["name"] for laptop in laptops if laptop["rating"] > 5]
-print(good_laptops)
-
+good_laptops = [laptop["name"] for laptop in laptops if int(laptop["rating"]) > 5]
 
 root.mainloop()
